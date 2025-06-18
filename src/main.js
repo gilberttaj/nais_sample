b69e59a2edc7492a12edd478e93306ed5ae81a91
gibztajada <gibz1432@gmail.com>
2025-06-18 13:08:01 +0800

    added aws amplify

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import Amplify from 'aws-amplify'
+import amplifyconfig from './amplifyconfiguration.json'
+
+Amplify.configure(amplifyconfig)
 
 const app = createApp(App)
 app.use(router)
